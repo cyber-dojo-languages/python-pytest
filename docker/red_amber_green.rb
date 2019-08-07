e45e4e798188996b144b163b8183f97977180d31
JonJagger <jon@jaggersoft.com>
2019-08-07 16:44:46 +0100

    improve red/amber differentiation in the red_amber_green.rb file holding the lambda

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,7 +1,26 @@
 
+# Python-pytest, like most test frameworks does not
+# set status to non-zero at appropriate times :-(
+# So you have to look at stdout/stderr.
+# The particular problem is distinguishing red from amber.
+# Below is a start at trapping three specific amber errors.
+# There are no doubt more to come.
+
 lambda { |stdout,stderr,status|
   output = stdout + stderr
-  return :red   if /=== FAILURES ===/.match(output)
+  return :amber if /=== ERRORS ===/.match(output)
+
+  %w(
+    Attribute
+    Type
+    Name
+  ).each do |syntax_error_prefix|
+     return :amber if
+       /=== FAILURES ===/.match(output) &&
+         Regexp.new(".*:[0-9]+: #{syntax_error_prefix}Error").match(output)
+
+  end
+
   return :green if /=== (\d+) passed/.match(output)
-  return :amber
+  return :red
 }
