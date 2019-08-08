659138317f0971572747dc61f3e686b337622e66
JonJagger <jon@jaggersoft.com>
2019-08-08 06:24:00 +0100

    add another amber error regex to red_amber_green.rb

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -3,22 +3,23 @@
 # set status to non-zero at appropriate times :-(
 # So you have to look at stdout/stderr.
 # The particular problem is distinguishing red from amber.
-# Below is a start at trapping three specific amber errors.
-# There are no doubt more to come.
+# Below is a start at trapping some specific amber errors.
+# There are no doubt more.
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
+  
   return :amber if /=== ERRORS ===/.match(output)
 
   %w(
     Attribute
-    Type
     Name
+    Type
+    UnboundLocal
   ).each do |syntax_error_prefix|
      return :amber if
        /=== FAILURES ===/.match(output) &&
          Regexp.new(".*:[0-9]+: #{syntax_error_prefix}Error").match(output)
-
   end
 
   return :green if /=== (\d+) passed/.match(output)
