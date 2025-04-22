07cc074aa6f93d29fc8ac99bad612a5af76a6345
Faye <faye@kosli.com>
2025-04-22 14:25:18 +0200

    Upgrade to python 3.13.3 and pytest 8.3.5

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=8.3.4
+readonly EXPECTED=8.3.5
 readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} sh -c 'pytest --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
