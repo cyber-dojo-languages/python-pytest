b2d30d3ade06c4525084e128f6aba6f76c5e8b5d
JonJagger <jon@kosli.com>
2025-11-16 19:38:15 +0000

    Update pytest to 9.0.1

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=8.4.2
+readonly EXPECTED=9.0.1
 readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} sh -c 'pytest --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
